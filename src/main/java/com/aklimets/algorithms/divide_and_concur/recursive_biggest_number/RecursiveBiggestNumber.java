25e19ed99ded95b8c4247e050e3111e1ea83a823
aliaksandrklimets <aliaksandrklimets@gmail.com>
2024-12-23 14:49:01 +0100

    Add recursive biggest number task

---
+++ src/main/java/com/aklimets/algorithms/divide_and_concur/recursive_biggest_number/RecursiveBiggestNumber.java
@@ -0,0 +1,16 @@
+package com.aklimets.algorithms.divide_and_concur.recursive_biggest_number;
+
+import java.util.Arrays;
+
+public class RecursiveBiggestNumber {
+
+    public int solve(int[] input) {
+        if (input.length == 1) {
+            return input[0];
+        }
+
+        int[] left = Arrays.copyOfRange(input, 0, input.length / 2);
+        int[] right = Arrays.copyOfRange(input, (input.length / 2), input.length);
+        return Math.max(solve(left), solve(right));
+    }
+}
--- src/test/java/com/aklimets/algorithms/divide_and_concur/recursive_biggest_number/RecursiveBiggestNumberUnitTest.java
+++ src/test/java/com/aklimets/algorithms/divide_and_concur/recursive_biggest_number/RecursiveBiggestNumberUnitTest.java
@@ -1,4 +1,4 @@
-package com.aklimets.algorithms.divide_and_concur.recursive_array_sum;
+package com.aklimets.algorithms.divide_and_concur.recursive_biggest_number;
 
 import com.aklimets.helper.TestHelper;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -6,13 +6,12 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class RecursiveArraySumUnitTest {
+class RecursiveBiggestNumberUnitTest {
 
-    private final RecursiveArraySum cut = new RecursiveArraySum();
-
+    private final RecursiveBiggestNumber cut = new RecursiveBiggestNumber();
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/arrays/array_sum.csv", numLinesToSkip = 1)
+    @CsvFileSource(resources = "/arrays/biggest_number.csv", numLinesToSkip = 1)
     public void shouldPassTest_whenSolve_given_input(String input, int expected) {
         // given
         int[] inputArray = TestHelper.splitInput(input);
