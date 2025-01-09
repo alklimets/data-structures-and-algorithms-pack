4b0e5e7d95e9cf2d0b030a2e1399e0d6dcdec651
aliaksandrklimets <aliaksandrklimets@gmail.com>
2025-01-09 16:31:01 +0100

    Solved few challenges

---
+++ src/main/java/com/aklimets/algorithms/greedy/neetcode/max_subarray/jump_game_2/JumpGame2.java
@@ -0,0 +1,49 @@
+package com.aklimets.algorithms.greedy.neetcode.max_subarray.jump_game_2;
+
+public class JumpGame2 {
+
+    /*
+        Jump Game II
+        You are given an array of integers nums, where nums[i] represents the maximum length of a jump towards the right from index i. For example, if you are at nums[i], you can jump to any index i + j where:
+
+        j <= nums[i]
+        i + j < nums.length
+        You are initially positioned at nums[0].
+
+        Return the minimum number of jumps to reach the last position in the array (index nums.length - 1). You may assume there is always a valid answer.
+
+        Example 1:
+
+        Input: nums = [2,4,1,1,1,1]
+
+        Output: 2
+        Explanation: Jump from index 0 to index 1, then jump from index 1 to the last index.
+
+        Example 2:
+
+        Input: nums = [2,1,2,1,0]
+
+        Output: 2
+        Constraints:
+
+        1 <= nums.length <= 1000
+        0 <= nums[i] <= 100
+    */
+
+    public int jump(int[] nums) {
+        int ind = nums.length - 2, goal = ind + 1;
+        int res = 0;
+        while (ind >= 0) {
+            int minIndex = ind;
+            for (int i = ind; i >= 0; i--) {
+                if (nums[i] >= goal - i && i < minIndex) {
+                    minIndex = i;
+                }
+            }
+            res++;
+            goal = minIndex;
+            ind = goal - 1;
+        }
+        return res;
+    }
+}
