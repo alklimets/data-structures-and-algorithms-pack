ee23753532b54b45da3880e3e0a0e2b2d8222b76
aliaksandrklimets <aliaksandrklimets@gmail.com>
2025-01-12 20:09:52 +0100

    Solved few challenges

---
+++ src/main/java/com/aklimets/challenges/neetcode/bit_manipulation/number_of_one_bits/NumberOfOneBits.java
@@ -0,0 +1,31 @@
+package com.aklimets.challenges.neetcode.bit_manipulation.number_of_one_bits;
+
+public class NumberOfOneBits {
+
+    /*
+        Number of One Bits
+        You are given an unsigned integer n. Return the number of 1 bits in its binary representation.
+
+        You may assume n is a non-negative integer which fits within 32-bits.
+
+        Example 1:
+
+        Input: n = 00000000000000000000000000010111
+
+        Output: 4
+        Example 2:
+
+        Input: n = 01111111111111111111111111111101
+
+        Output: 30
+    */
+
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            if (n % 2 == 1) count++;
+            n>>=1;
+        }
+        return count;
+    }
+}
