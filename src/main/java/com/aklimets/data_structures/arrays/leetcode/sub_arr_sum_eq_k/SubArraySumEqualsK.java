139bd16d87fa2da11bf702c158b0413e51420513
aliaksandrklimets <aliaksandrklimets@gmail.com>
2025-01-14 13:18:26 +0100

    Solved few challenges

--- src/main/java/com/aklimets/data_structures/arrays/leetcode/sub_arr_sum_eq_k/SubArraySumEqualsK.java
+++ src/main/java/com/aklimets/data_structures/arrays/leetcode/sub_arr_sum_eq_k/SubArraySumEqualsK.java
@@ -1,5 +1,8 @@
 package com.aklimets.data_structures.arrays.leetcode.sub_arr_sum_eq_k;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubArraySumEqualsK {
     public static int subarraySum(int[] nums1, int k) {
         int sum = 0;
@@ -7,12 +10,11 @@ public class SubArraySumEqualsK {
         for (int i = 1; i < nums.length; i++) {
             nums[i] = nums1[i - 1] + nums[i - 1];
         }
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                int diff = nums[j] - nums[i - 1];
-                if (diff == k) sum++;
-            }
 
+        Map<Integer, Integer> sums = new HashMap<>();
+        for (int num : nums) {
+            if (sums.containsKey(num - k)) sum += sums.get(num - k);
+            sums.compute(num, (key, value) -> value == null ? 1 : value + 1);
         }
 
         return sum;
