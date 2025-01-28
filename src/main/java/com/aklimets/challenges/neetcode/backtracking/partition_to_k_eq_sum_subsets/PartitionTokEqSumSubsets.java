b8fc8a0b0e796e54747e3dc9184424bb80155813
aliaksandrklimets <aliaksandrklimets@gmail.com>
2025-01-28 14:39:44 +0100

    Solved few challenges

---
+++ src/main/java/com/aklimets/challenges/neetcode/backtracking/partition_to_k_eq_sum_subsets/PartitionTokEqSumSubsets.java
@@ -0,0 +1,51 @@
+package com.aklimets.challenges.neetcode.backtracking.partition_to_k_eq_sum_subsets;
+
+public class PartitionTokEqSumSubsets {
+
+    /*
+        Given an integer array nums and an integer k, return true if it is possible to divide this array into k non-empty subsets whose sums are all equal.
+        Example 1:
+        Input: nums = [4,3,2,3,5,2,1], k = 4
+        Output: true
+        Explanation: It is possible to divide it into 4 subsets (5), (1, 4), (2,3), (2,3) with equal sums.
+
+        Example 2:
+        Input: nums = [1,2,3,4], k = 3
+        Output: false
+
+        Constraints:
+
+        1 <= k <= nums.length <= 16
+        1 <= nums[i] <= 10^4
+        The frequency of each element is in the range [1, 4].
+    */
+
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+
+        if (sum % k > 0) return false;
+        return dfs(nums, k, 0, new boolean[nums.length], 0, sum / k);
+    }
+
+    private boolean dfs(int[] nums, int k, int start, boolean[] visited, int sum, int target) {
+        if (k == 0) return true;
+
+        if (sum == target) {
+            return dfs(nums, k - 1, 0, visited, 0, target);
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            if (visited[i]) continue;
+            if (sum + nums[i] > target) continue;
+
+            sum += nums[i];
+            visited[i] = true;
+            if (dfs(nums, k, i + 1, visited, sum, target)) return true;
+            sum -= nums[i];
+            visited[i] = false;
+
+        }
+        return false;
+    }
+}
