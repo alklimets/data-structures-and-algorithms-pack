a4ec4e73a517ef1cba440af4e85332bf9035389b
aliaksandrklimets <aliaksandrklimets@gmail.com>
2025-02-18 01:10:09 +0100

    Solved few challenges

--- src/main/java/com/aklimets/data_structures/arrays/leetcode/first_missing_positive/FirstMissingPositive.java
+++ src/main/java/com/aklimets/data_structures/arrays/leetcode/first_missing_positive/FirstMissingPositive.java
@@ -48,4 +48,26 @@ public class FirstMissingPositive {
         }
         return max + 1;
     }
+
+    public int firstMissingPositive2(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[i];
+                nums[i] = nums[nums[i] - 1];
+                nums[temp - 1] = temp;
+                System.out.println();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FirstMissingPositive().firstMissingPositive2(new int[] {0,3,2}));
+    }
 }
