93c90fd434a3a1fa00aff2c5fe1d86a886dfe077
aliaksandrklimets <aliaksandrklimets@gmail.com>
2025-02-26 16:35:52 +0100

    Solved few challenges

--- src/main/java/com/aklimets/data_structures/trees/TreeNode.java
+++ src/main/java/com/aklimets/data_structures/trees/TreeNode.java
@@ -1,5 +1,8 @@
 package com.aklimets.data_structures.trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
 
     public int val;
@@ -17,5 +20,38 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public static TreeNode create(Queue<Integer> queue) {
+        if (queue.isEmpty()) {
+            return null;
+        }
+
+        Integer val = queue.poll();
+        TreeNode root = new TreeNode(val);
+
+        Queue<TreeNode> nodes = new LinkedList<>();
+        nodes.offer(root);
+        create(queue, nodes);
+        return root;
+    }
+
+    private static void create(Queue<Integer> queue, Queue<TreeNode> nodes) {
+        while (!queue.isEmpty() && !nodes.isEmpty()) {
+            for (int i = nodes.size(); i > 0; i--) {
+                TreeNode poll = nodes.poll();
+                if (!queue.isEmpty()) {
+                    Integer num = queue.poll();
+                    poll.left = new TreeNode(num);
+                    nodes.offer(poll.left);
+                }
+
+                if (!queue.isEmpty()) {
+                    Integer num = queue.poll();
+                    poll.right = new TreeNode(num);
+                    nodes.offer(poll.right);
+                }
+            }
+        }
     }
 }
